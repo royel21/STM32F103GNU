ca9f14848ace2980051b593c500bcc576252ee6c
RoyerAriel <RoyerAriel@192.168.80.1>
2016-12-11 18:23:48 -0400

    -ADC working with DMA

--- STM32F103GNU/Lib/include/Analog.h
+++ STM32F103GNU/Lib/include/Analog.h
@@ -15,36 +15,49 @@
 extern "C" {
 #endif
 
-/********************  Bits definition for DMA_SxCR register  *****************/
-#define DMA_CCR_PL_VH                       ((uint16_t)0x0003000)
-#define DMA_CCR_PL_M                        ((uint16_t)0x0001000)
-#define DMA_CCR_PL_H                        ((uint16_t)0x0002000)
-#define DMA_CCR_MSIZE_8bit                  ((uint16_t)0x0000000)
-#define DMA_CCR_MSIZE_16bit                 ((uint16_t)0x0000400)
-#define DMA_CCR_MSIZE_32bit                 ((uint16_t)0x0000800)
-#define DMA_CCR_PSIZE_8bit                  ((uint16_t)0x0000000)
-#define DMA_CCR_PSIZE_16bit                 ((uint16_t)0x0000100)
-#define DMA_CCR_PSIZE_32bit                 ((uint16_t)0x0000200)
-#define DMA_CCR_DIR_P2M                     ((uint16_t)0x0000000)
-#define DMA_CCR_DIR_M2P                     ((uint16_t)0x0000040)
-#define DMA_CCR_DIR_M2M                     ((uint16_t)0x0000080)
-#define CH0                                 ((uint8_t)0x00)
-#define CH1                                 ((uint8_t)0x01)
-#define CH2                                 ((uint8_t)0x02)
-#define CH3                                 ((uint8_t)0x03)
-#define CH4                                 ((uint8_t)0x04)
-#define CH5                                 ((uint8_t)0x05)
-#define CH6                                 ((uint8_t)0x06)
-#define CH7                                 ((uint8_t)0x07)
-#define CH8                                 ((uint8_t)0x08)
-#define CH9                                 ((uint8_t)0x09)
+#define ADC_CH0                                 ((uint8_t)0x00)
+#define ADC_CH1                                 ((uint8_t)0x01)
+#define ADC_CH2                                 ((uint8_t)0x02)
+#define ADC_CH3                                 ((uint8_t)0x03)
+#define ADC_CH4                                 ((uint8_t)0x04)
+#define ADC_CH5                                 ((uint8_t)0x05)
+#define ADC_CH6                                 ((uint8_t)0x06)
+#define ADC_CH7                                 ((uint8_t)0x07)
+#define ADC_CH8                                 ((uint8_t)0x08)
+#define ADC_CH9                                 ((uint8_t)0x09)
+
+#define ADC_C1_5                                ((uint8_t)0x00)
+#define ADC_C7_5                                ((uint8_t)0x01)
+#define ADC_C13_5                               ((uint8_t)0x02)
+#define ADC_C28_5                               ((uint8_t)0x03)
+#define ADC_C41_5                               ((uint8_t)0x04)
+#define ADC_C55_5                               ((uint8_t)0x05)
+#define ADC_C71_5                               ((uint8_t)0x06)
+#define ADC_C239_5                              ((uint8_t)0x07)
+
 
 class ADC
 {
-	public:
-		ADC(ADC_TypeDef *ADC);
-		void readWithDMAMulti(uint16_t pins, volatile uint16_t *vars, uint8_t size);
-		uint16_t read(uint16_t);
+  private:
+    volatile uint16_t data[9] =
+      { };
+    uint8_t size = 0;
+    ADC_TypeDef *adc;
+  public:
+    ADC(ADC_TypeDef *ADC);
+
+    void setDMA();
+
+    void powerOn(uint8_t on);
+
+    void setContMode(uint8_t state);
+
+    uint16_t getdata(uint8_t ch);
+
+    void setChannel(uint8_t ch, uint8_t cicles = 0);
+
+    void startConV();
+    uint16_t read(uint8_t ch);
 };
 
 #ifdef __cplusplus
--- STM32F103GNU/Lib/src/Analog.cpp
+++ STM32F103GNU/Lib/src/Analog.cpp
@@ -6,95 +6,114 @@
  */
 #include "Analog.h"
 #include "Util.h"
+/********************  Bits definition for DMA_SxCR register  *****************/
+#define DMA_CCR_PL_VH                       ((uint16_t)0x0003000)
+#define DMA_CCR_PL_M                        ((uint16_t)0x0001000)
+#define DMA_CCR_PL_H                        ((uint16_t)0x0002000)
+#define DMA_CCR_MSIZE_8bit                  ((uint16_t)0x0000000)
+#define DMA_CCR_MSIZE_16bit                 ((uint16_t)0x0000400)
+#define DMA_CCR_MSIZE_32bit                 ((uint16_t)0x0000800)
+#define DMA_CCR_PSIZE_8bit                  ((uint16_t)0x0000000)
+#define DMA_CCR_PSIZE_16bit                 ((uint16_t)0x0000100)
+#define DMA_CCR_PSIZE_32bit                 ((uint16_t)0x0000200)
+#define DMA_CCR_DIR_P2M                     ((uint16_t)0x0000000)
+#define DMA_CCR_DIR_M2P                     ((uint16_t)0x0000040)
+#define DMA_CCR_DIR_M2M                     ((uint16_t)0x0000080)
 
 ADC::ADC(ADC_TypeDef *ADC)
 {
-	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
-	uint32_t temp = (RCC->CFGR & 0xFFFF3FFF);
-	temp |= RCC_CFGR_ADCPRE_DIV6;
-	RCC->CFGR = temp;
-	//Reset all ADC calibration register and wait to finish
-	ADC->CR2 |= ADC_CR2_RSTCAL;
-	while (ADC->CR2 & ADC_CR2_RSTCAL)
-		;
-	//start ADC calibration and wait to finish
-	ADC->CR2 |= ADC_CR2_CAL;
-	while (ADC->CR2 & ADC_CR2_CAL)
-		;
+  adc = ADC;
+  RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+  adc->CR2 |= ADC_CR2_ADON;
+  uint32_t temp = (RCC->CFGR & 0xFFFF3FFF);
+  temp |= RCC_CFGR_ADCPRE_DIV6;
+  RCC->CFGR = temp;
+  //Reset all ADC calibration register and wait to finish
+  ADC->CR2 |= ADC_CR2_RSTCAL;
+  while (ADC->CR2 & ADC_CR2_RSTCAL)
+    ;
+  //start ADC calibration and wait to finish
+  ADC->CR2 |= ADC_CR2_CAL;
+  while (ADC->CR2 & ADC_CR2_CAL)
+    ;
 }
 
-void ADC::readWithDMAMulti(uint16_t pins, volatile uint16_t *vars, uint8_t size)
+void ADC::powerOn(uint8_t on)
 {
-	RCC->AHBENR |= RCC_AHBENR_DMA1EN;
-	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
-//DMA Config
-	DMA1_Channel1->CCR |= DMA_CCR1_CIRC | DMA_CCR1_MINC |
-	DMA_CCR_PSIZE_16bit | DMA_CCR1_MSIZE_0 | DMA_CCR_PL_VH;
-
-	DMA1_Channel1->CMAR = (uint32_t) &vars[0];
-
-	DMA1_Channel1->CPAR = (uint32_t) &ADC1->DR;
-
-	DMA1_Channel1->CNDTR = size;
-
-	DMA1_Channel1->CCR |= DMA_CCR1_EN;
-
-	InterruptEnabler(DMA1_Channel1_IRQn, 0, 0);
-
-	int8_t order = 0;
-	uint8_t pin = 0;
-	while (pins >> pin)
-	{
-		if ((pins & (1 << pin)))
-		{
-			if (pin < 8)
-			{
-				ADC1->SQR3 |= pin << (order * 5);
-				GPIOA->CRL &= ~(0xF << (pin * 4));
-			} else if (pin > 5 && pin < 12)
-			{
-				ADC1->SQR2 |= pin << (order * 5);
-				GPIOB->CRL &= ~(0xF << ((pin - 8) * 4));
-			}
-			pin++;
-			order++;
-		}
-	}
-//ADC Config
-	ADC1->CR1 |= ADC_CR1_SCAN;
-//ADC1->CR2 |= ADC_CR2_DMA | ADC_CR2_DDS | ADC_CR2_CONT;
-	ADC1->SQR1 |= (size - 1) << 20;
-//Enable ADC conversion
-	ADC1->CR2 |= ADC_CR2_ADON;
-	ADC1->CR2 |= ADC_CR2_SWSTART;
+  on ? adc->CR2 |= ADC_CR2_ADON : adc->CR2 &= ~ADC_CR2_ADON;
 }
 
-uint16_t ADC::read(uint16_t ch)
+void ADC::setDMA()
 {
-	uint16_t var = 0;
-	if (ch < 8)
-	{
-		RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-		uint32_t temp = GPIOA->CRL;
-		temp &= ~(0x0F << (ch * 4));
-		GPIOA->CRL = temp;
-	} else
-	{
-		RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
-		uint32_t temp = GPIOB->CRL;
-		temp &= ~(0x0F << (ch - 8) * 4);
-		GPIOB->CRL = temp;
-	}
-	ADC1->SMPR2 = ADC_SMPR2_SMP0_0;
-	ADC1->SQR3 = ch;
-	ADC1->CR2 = ADC_CR2_ADON;
-	ADC1->CR2 |= ADC_CR2_SWSTART; //Start the conversion
-	uint8_t timeout = 255;
-	while (!(ADC1->SR & ADC_SR_EOC) && timeout--)
-		; //Processing the conversion
+  RCC->AHBENR |= RCC_AHBENR_DMA1EN;
+  //DMA Config
+  DMA1_Channel1->CCR |= DMA_CCR1_CIRC | DMA_CCR1_MINC |
+  DMA_CCR_PSIZE_16bit | DMA_CCR1_MSIZE_0 | DMA_CCR_PL_VH;
 
-	var = ADC1->DR & 0x0fff;
-	ADC1->CR2 = ADC1->SMPR2 = 0x00;
-	return var; //Return the converted var
+  DMA1_Channel1->CMAR = (uint32_t) &data;
+
+  DMA1_Channel1->CPAR = (uint32_t) &adc->DR;
+
+  adc->CR2 |= ADC_CR2_DMA;
+  //ADC Config
+  adc->CR1 |= ADC_CR1_SCAN;
+}
+
+void ADC::setContMode(uint8_t state)
+{
+  state ? adc->CR2 |= ADC_CR2_CONT : adc->CR2 &= ~ADC_CR2_CONT;
+}
+
+void ADC::setChannel(uint8_t ch, uint8_t cicles)
+{
+  if (ch < 8)
+  {
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+    adc->SQR3 &= ~(ch << (size * 5));
+    adc->SQR3 |= ch << (size * 5);
+    GPIOA->CRL &= ~(0xF << (ch * 4));
+  } else if (ch > 5 && ch < 12)
+  {
+    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+    adc->SQR2 &= ~(ch << (size * 5));
+    adc->SQR2 |= ch << (size * 5);
+    GPIOB->CRL &= ~(0xF << ((ch - 8) * 4));
+  }
+  adc->SMPR2 &= ~(cicles << (ch * 3));
+  adc->SMPR2 |= cicles << (ch * 3);
+  size++;
+}
+
+void ADC::startConV()
+{
+  adc->SQR1 = 0;
+  adc->SQR1 |= (size) << 20;
+  DMA1_Channel1->CNDTR = 0;
+  DMA1_Channel1->CNDTR = size + 1;
+  DMA1_Channel1->CCR |= DMA_CCR1_EN;
+  adc->CR2 |= ADC_CR2_ADON;
+  adc->CR2 |= ADC_CR2_SWSTART;
+}
+
+uint16_t ADC::read(uint8_t ch)
+{
+    return data[ch];
+//  if (ch < 8)
+//  {
+//    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+//    GPIOA->CRL &= ~(0xF << (ch * 4));
+//  } else
+//  {
+//    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+//    GPIOB->CRL &= ~(0xF << ((ch - 8) * 4));
+//  }
+//  adc->SMPR2 = ADC_C1_5;
+//  adc->SQR3 = ch;
+//  adc->CR2 |= ADC_CR2_ADON;
+//  adc->CR2 |= ADC_CR2_SWSTART; //Start the conversion
+//  while (!(adc->SR & ADC_SR_EOC))
+//    ; //Processing the conversion
+//  adc->CR2 &= ~ADC_CR2_ADON;
+//  return adc->DR & 0x0fff;
 }
 
